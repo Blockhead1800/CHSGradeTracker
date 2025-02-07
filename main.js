80990606e5cf094fe8d4092128fee2581212047d
Blockhead1800 <94411149+Blockhead1800@users.noreply.github.com>
2025-02-07 10:52:46 -0500

    Update main.js

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function updateThemeButton(theme) {
 }
 
 function initializeTheme() {
-  const savedTheme = localStorage.getItem('theme') || 'light';
+  const savedTheme = localStorage.getItem('theme') || 'dark';
   document.documentElement.setAttribute('data-theme', savedTheme);
   updateThemeButton(savedTheme);
 }
